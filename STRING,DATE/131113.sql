17bd0d6c94407d263a5426f0104ae5d400959dab
sweetburble <79733289+sweetburble@users.noreply.github.com>
2024-02-21 17:14:34 +0900

    2024/02/21 2번째 문제 풀이

--- STRING,DATE/131113.sql
+++ STRING,DATE/131113.sql
@@ -7,3 +7,15 @@ SELECT ORDER_ID,
        ELSE '출고미정'
        END AS '출고여부'
 FROM FOOD_ORDER
+
+/*
+그냥 옛날에 푼거라 마음에 안들음
+*/
+SELECT ORDER_ID, PRODUCT_ID, DATE_FORMAT(OUT_DATE, '%Y-%m-%d') AS OUT_DATE,
+    CASE 
+        WHEN DATE_FORMAT(OUT_DATE, '%Y-%m-%d') > '2022-05-01' THEN '출고대기'
+        WHEN DATE_FORMAT(OUT_DATE, '%Y-%m-%d') <= '2022-05-01' THEN '출고완료'
+        ELSE '출고미정'
+    END AS '출고여부'
+FROM FOOD_ORDER
+ORDER BY ORDER_ID
